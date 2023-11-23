bd9364f0ad8d64cc8159bc1a4cabe11b6a506dd1
Rosa Valdez <rosa.m42@icloud.com>
2023-11-23 12:33:22 -0600

    fixed errors

--- index.js
+++ index.js
@@ -1,90 +1,53 @@
 const inquirer = require("inquirer");
-const fs= require ("fs");
+const Svg = required('./lib/svg');
+const fs = require ("fs");
 const {Circle, Square, Triangle} = require("./lib/shapes");
-const {userInput, getInput} =require("./lib/input");
+const {userInput, getInput} =require("./lib/svg");
+const { error } = required("console");
 
 // prompts with questions
-inquirer.prompt([
+const questions = [
     {
+        type: "input",
         name:"text",
         message:"Enter up to three characters.",
         type: "input",
-        filter: function(input){
-            return input.trim().toUpperCase();
-        },
-        Validate:function(input) { 
-            if (input.length === 0 || input.length > 3) {
-                return "What three letters are the text for your logo?";
-            } else {
-                return true;
-            }
-          },
-        },
-      {
-        name:"textColor",
-        message:"What color is your log? (Bykeyword or hexadecimal number)",
+        validate:(input) => input.length <= 3,
+    },
         type:"input",
-        filter: function(input){
-            return input.trim().toLowerCase();
-        },
-        validate: validateColorName,
-      },
-      {
+        name: "textColor",
+        message:"What color is your log? (Bykeyword or hexadecimal number)",
+    },
+    {
+        type: "list",
         name: 'logoShape',
         message: "What shape is yout logo?",
-        type: 'list',
-        choices: [
-            {
-                name: 'Circle',
-                value: new Circle(),
-            },
-            {
-                name: 'Square',
-                value: new Square(),
-            },
-            {
-                name: 'Triangle',
-                value: new Triangle(),
-            }
-        ]
+        choices:["circle", "triangle, "square"],
     },
-        name: "shapeColor",
-        message: "What color is your shape? (By keyword or hexadecimal number)",
         type: "input",
-        filter: function(input){
-            return input.trim(). toLowerCase();
-        },
-        validate: varlidateColorName,
-        },
-    ]).then(answer => {
-        console.log(answer);
-
-        svgMaker(answer);
+        name: "shapeColor",
+        message: "What color is your shape? (By keyword or hexadecimal number)",    
+    },
+  ];
+  inquirer
+    .createPromptModule(questions)
+    .then((responses) => {
+      let shape;
+      if (response.shape === "circle"){ 
+      } else if (response.shape === "triangle"){
+        shape = new Triangle();
+      } else {
+        shape = new Square();
+      }
+      shape.setLogoColor(response.shapeColor);  
+      const svg = new Svg();
+      svg.setShape(shape);
+      svg.setText(response.text, response.textColor);
+      fs.writeFile(`examples/logo-${response.text}.svg`, svg.render(), (err) => {
+        if (err) console.log(err);
+          console.log("Your logo was created!!");
+      });
     })
-//function processes data for creating svg file
-    function svgMaker( { text, textColor, logoShape, logoColor } ) {
-        logoShape.setLogoColor(logoColor);
-        const svgContent = `
-            <svg version="1.1"
-                width="300" height="200"
-                xmlns="http://www.w3.org/2000/svg">
-                <rect width="100%" height="100%" fill="transparent" />
-
-                ${logoShape.renderLogoColor()}
-
-                ${logoShape.rendertext(text, logoColor)}
-            </svg>`;
-
-    writeToFile('logo.svg', svgContent);
-  }
-  //function generates files based on received data
-    function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, (error) => {
-            if (error) {
-                console.log(error);
-                return;
-            } else {
-                console.log('Generated logo.svg!');
-            }
-        })
-      };
+    .catch((err) => {
+      console.log(err.message);
+    });
