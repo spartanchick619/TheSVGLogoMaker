0182f66d817f2493c863972388fad0759fc5fa21
Rosa Valdez <rosa.m42@icloud.com>
2023-11-23 18:07:38 -0600

    fixed errros

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class Svg {
       this.shapeElement = ''
     }
     render(){
-      return <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>
+      return <Svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></Svg>
     }
     setTextElement(text,color){
       this.textElement = ' <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>'
